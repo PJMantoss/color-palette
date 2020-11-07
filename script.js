2bd16ea17f89af5236d6160d202c82694811c0d0
pjmantoss <joelptoss@gmail.com>
2020-11-07 19:07:21 +0100

    update script

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ async function getColors(colorCount){
     let response = await fetch(`https://api.noopschallenge.com/hexbot?count=${colorCount}`)
 
     let data = await response.json();
+
+    let colors = data.color;
+
+    displayColors(colors);
 }
+
+let colorCount = 100;
+getColors(colorCount);
